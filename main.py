d53b67fe2dc2610696d74c02601f2c028aa8d366
ShahabAthar25 <shahabathar25@gmail.com>
2021-09-20 11:58:20 +0500

    Add Face Detection

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ while True:
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(frame, scaleFactor=1.5, minNeighbors=5)
-    smile = smile_cascade.detectMultiScale(frame, scaleFactor=1.7, minNeighbors=70)
+    smile = smile_cascade.detectMultiScale(frame, scaleFactor=2, minNeighbors=70)
 
-    # for (x, y, w, h) in faces:
-    #     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 0), 2)
 
     for (x, y, w, h) in smile:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
